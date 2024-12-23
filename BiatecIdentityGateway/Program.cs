8794b4f91d33814f5dea36c2eb18bb4d295b151d
Ludovit Scholtz <ludko2@gmail.com>
2024-12-23 17:40:56 +0100

    cors, auth, derec service check before startup

--- BiatecIdentityGateway/Program.cs
+++ BiatecIdentityGateway/Program.cs
@@ -1,4 +1,6 @@
+using AlgorandAuthentication;
 using BiatecIdentityGateway.BusinessController;
+using Grpc.Net.Client;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -32,12 +34,60 @@ builder.Services.AddSingleton<Gateway>();
 builder.Services.Configure<BiatecIdentityGateway.Model.Config>(
     builder.Configuration.GetSection("GatewayConfig"));
 
+var algorandAuthenticationOptions = new AlgorandAuthenticationOptions();
+builder.Configuration.GetSection("AlgorandAuthentication").Bind(algorandAuthenticationOptions);
+builder.Services
+ .AddAuthentication(AlgorandAuthenticationHandler.ID)
+ .AddAlgorand(o =>
+ {
+     o.CheckExpiration = algorandAuthenticationOptions.CheckExpiration;
+     o.Debug = algorandAuthenticationOptions.Debug;
+     o.AlgodServer = algorandAuthenticationOptions.AlgodServer;
+     o.AlgodServerToken = algorandAuthenticationOptions.AlgodServerToken;
+     o.AlgodServerHeader = algorandAuthenticationOptions.AlgodServerHeader;
+     o.Realm = algorandAuthenticationOptions.Realm;
+     o.NetworkGenesisHash = algorandAuthenticationOptions.NetworkGenesisHash;
+     o.MsPerBlock = algorandAuthenticationOptions.MsPerBlock;
+     o.EmptySuccessOnFailure = algorandAuthenticationOptions.EmptySuccessOnFailure;
+     o.EmptySuccessOnFailure = algorandAuthenticationOptions.EmptySuccessOnFailure;
+ });
+
+// Add CORS policy
+var corsConfig = builder.Configuration.GetSection("Cors").AsEnumerable().Select(k => k.Value).Where(k => !string.IsNullOrEmpty(k)).ToArray();
+if (!(corsConfig?.Length > 0)) throw new Exception("Cors not defined");
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+    builder =>
+    {
+        builder.WithOrigins(corsConfig)
+                            .SetIsOriginAllowedToAllowWildcardSubdomains()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials()
+                            .WithExposedHeaders("rowcount", "rowstate");
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+using (var channel = GrpcChannel.ForAddress("http://localhost:50051"))
+{
+    // this tests the connection to the derec crypto service and fails to start the app if the connection does not exists. it also allows to create new app configuration if not defined
+    var client = new DerecCrypto.DeRecCryptographyService.DeRecCryptographyServiceClient(channel);
+    var encKeys = client.EncryptGenerateEncryptionKey(new DerecCrypto.EncryptGenerateEncryptionKeyRequest());
+    
+    app.Logger.LogInformation($"New enc keys: PK: {encKeys.PublicKey.ToBase64()} SK: {encKeys.PrivateKey.ToBase64()} ");
+    var signKeys = client.SignGenerateSigningKey(new DerecCrypto.SignGenerateSigningKeyRequest());
+    app.Logger.LogInformation($"New sign keys: PK: {signKeys.PublicKey.ToBase64()} SK: {signKeys.PrivateKey.ToBase64()} ");
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseCors();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
